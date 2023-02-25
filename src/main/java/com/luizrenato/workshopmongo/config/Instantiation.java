4295da6a5b71ae09e3479a9b7fecdea485c4f1f9
Luiz Renato <renatoperez107@gmail.com>
2023-02-25 17:13:41 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/luizrenato/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.luizrenato.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.luizrenato.workshopmongo.domain.User;
+import com.luizrenato.workshopmongo.repository.UserRepository;
+
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		userReposiroty.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
+
